8f2d768f163c22158a4d0a4f16ec91ee190a9fee
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-12-25 17:17:31 +0100

    return all vehicles

--- immatriculation-service/immatriculation-query-service/src/main/java/me/elmajni/immatriculationqueryservice/controllers/VehicleQueryController.java
+++ immatriculation-service/immatriculation-query-service/src/main/java/me/elmajni/immatriculationqueryservice/controllers/VehicleQueryController.java
@@ -1,10 +1,13 @@
 package me.elmajni.immatriculationqueryservice.controllers;
 
 import lombok.AllArgsConstructor;
+import me.elmajni.commonapi.GetAllRadarsQuery;
 import me.elmajni.commonapi.GetAllVehiclesQuery;
 import me.elmajni.commonapi.GetVehicleByRegistrationNumber;
+import me.elmajni.commonapi.RadarResponseDTO;
 import me.elmajni.immatriculationqueryservice.entities.Vehicle;
 import me.elmajni.immatriculationqueryservice.entities.VehicleOwner;
+import me.elmajni.immatriculationqueryservice.repositories.VehicleRepository;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.stereotype.Service;
@@ -14,20 +17,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/query/vehicule")
+@RequestMapping("/query/vehicle")
 @AllArgsConstructor
 @Service
 public class VehicleQueryController {
 
         private QueryGateway queryGateway;
+    private final VehicleRepository vehicleRepository;
 
 
-        @GetMapping(path = "/")
-        public List<Vehicle> getVehicules() {
-            return queryGateway.query(new GetAllVehiclesQuery(), ResponseTypes.multipleInstancesOf(Vehicle.class)).join();
+    @GetMapping(path = "/all")
+        public CompletableFuture<List<Vehicle>> getVehicles() {
+            /*return queryGateway.query(new GetAllVehiclesQuery(),
+                    ResponseTypes.multipleInstancesOf(Vehicle.class)).join();
+             */
+            return queryGateway.query(
+                    new GetAllVehiclesQuery()
+                    , ResponseTypes.multipleInstancesOf(Vehicle.class)
+            );
         }
+
 
         @GetMapping(path = "/{id}")
         public VehicleOwner getVehicule(@PathVariable String id) {
@@ -38,4 +51,6 @@ public class VehicleQueryController {
         public List<InfractionResponseDTO> getVehiculeByMatricule(@PathVariable String number) {
             return queryGateway.query(new GetInfractionsByVehicle(matricule), ResponseTypes.multipleInstancesOf(InfractionResponseDTO.class)).join();
         }*/
+
+
 }
